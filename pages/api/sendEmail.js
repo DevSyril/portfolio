286b781866316210328caceb013938556a636bc0
Devsyril <154521714+DevSyril@users.noreply.github.com>
2024-12-18 16:05:33 +0000

    Modification de la clé d'API

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,6 +1,6 @@
 import sendgrid from '@sendgrid/mail';
 
-sendgrid.setApiKey('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');
+sendgrid.setApiKey('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');
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
